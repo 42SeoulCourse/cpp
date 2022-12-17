06eb1afdeeb28b470b41858b26b19527bacdb667
김재환 <woghks7859@gmail.com>
2022-12-17 17:50:50 +0900

    REFACT: cpp01 ex05 clang-format 적용

--- cpp01/ex05/Harl.hpp
+++ cpp01/ex05/Harl.hpp
@@ -1,22 +1,21 @@
 #ifndef HARL_HPP
-# define HARL_HPP
+#define HARL_HPP
 
 #include <iostream>
 
-class Harl
-{
-	private:
-		void	debug( void );
-		void	info( void );
-		void	error( void );
-		void	warning( void );
-		void	invalidLevel( std::string level );
+class Harl {
+ private:
+  void debug(void);
+  void info(void);
+  void error(void);
+  void warning(void);
+  void invalidLevel(std::string level);
 
-	public:
-		Harl( void );
-		~Harl( void );
+ public:
+  Harl(void);
+  ~Harl(void);
 
-		void	complain( std::string level );
+  void complain(std::string level);
 };
 
 #endif
