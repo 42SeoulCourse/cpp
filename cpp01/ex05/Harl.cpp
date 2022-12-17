db02dc256adc489fdd90811a7df07f9db62b4ffe
김재환 <woghks7859@gmail.com>
2022-12-17 18:28:21 +0900

    CHORE: 불필요한 세미콜론 삭제

--- cpp01/ex05/Harl.cpp
+++ cpp01/ex05/Harl.cpp
@@ -16,7 +16,6 @@ void Harl::info(void) {
       << "I cannot believe adding extra bacon costs more money. You didn’t put "
          "enough bacon in my burger! If you did, I wouldn’t be asking for more!"
       << std::endl;
-  ;
 }
 
 void Harl::warning(void) {
@@ -24,13 +23,11 @@ void Harl::warning(void) {
       << "I think I deserve to have some extra bacon for free. I’ve been "
          "coming for years whereas you started working here since last month."
       << std::endl;
-  ;
 }
 
 void Harl::error(void) {
   std::cout << "This is unacceptable! I want to speak to the manager now."
             << std::endl;
-  ;
 }
 
 void Harl::invalidLevel(std::string level) {
