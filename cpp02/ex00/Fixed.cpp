e9cfe4fcde6aff5e9409925d6ff0fcd24e02c818
김재환 <woghks7859@gmail.com>
2022-12-20 13:33:27 +0900

    FEAT: cpp02 ex00 구현

---
+++ cpp02/ex00/Fixed.cpp
@@ -0,0 +1,32 @@
+#include "./Fixed.hpp"
+
+Fixed::Fixed(void) {
+  std::cout << "Default constructor called" << std::endl;
+  return;
+}
+
+Fixed::~Fixed(void) {
+  std::cout << "Destructor called" << std::endl;
+  return;
+}
+
+Fixed::Fixed(Fixed &fixed) {
+  std::cout << "Copy constructor called" << std::endl;
+  *this = fixed;
+}
+
+Fixed &Fixed::operator=(Fixed &fixed) {
+  std::cout << "Copy assignment operator called" << std::endl;
+  this->_integer = fixed.getRawBits();
+  return (*this);
+}
+
+int Fixed::getRawBits(void) const {
+  std::cout << "getRawBits member function called" << std::endl;
+  return (this->_integer);
+}
+
+void  Fixed::setRawBits(int const raw) {
+  std::cout << "setRawBits member function called" << std::endl;
+  this->_integer = raw;
+}
