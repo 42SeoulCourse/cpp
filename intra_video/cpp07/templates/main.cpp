57f8c91e088bc4917998116ff23e72174e451327
김재환 <woghks7859@gmail.com>
2023-01-07 20:38:37 +0900

    STUDY: template 공부

---
+++ intra_video/cpp07/templates/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+template< typename T >
+class List {
+     public:
+          List<T>(T const &content) {
+               // 
+          }
+
+          List<T>(List<T> const &list) {
+               // 
+          }
+
+          ~List<T>( void ) {
+               // 
+          }
+     
+     private:
+      T *_content;
+      List<T> *_next;
+};
+
+int main(void) {
+     List<int> a(42);
+     List<float> b(3.24f);
+     List<List<int>> c(a);
+
+     return 0;
+}
