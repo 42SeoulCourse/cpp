a382d7ccff0da05b5fac868c567f8320b4870568
김재환 <woghks7859@gmail.com>
2023-01-09 19:13:30 +0900

    FEAT: cpp07 ex01 구현

---
+++ cpp07/ex01/iter.hpp
@@ -0,0 +1,22 @@
+#ifndef ITER_HPP
+#define ITER_HPP
+
+#include <cstring>
+#include <iostream>
+
+template <typename T>
+void iter(T *arr, size_t len, void (*f)(T const &value)) {
+  if (!arr || len == 0 || !f) {
+    return;
+  }
+  for (size_t i = 0; i < len; i++) {
+    f(arr[i]);
+  }
+}
+
+template <typename T>
+void print(T const &x) {
+  std::cout << x << std::endl;
+}
+
+#endif
---
+++ cpp07/ex01/main.cpp
@@ -0,0 +1,6 @@
+#include "iter.hpp"
+
+int main(){
+	std::string arr[3] = {"aaa", "bbb", "ccc"};
+	iter(arr, 3, print);
+}
